23244ba618f02e849c8b4fa4e326fcbcbe935148
Muhamad Abubakar <59134278+Abubakar123421@users.noreply.github.com>
2026-03-18 04:37:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app = FastAPI(title="Bot Dashboard API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://bot-dashboard-iota.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
